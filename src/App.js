897aa5abf0126eaaccbb3e4dbf6c023ae812f3fd
pratiksharm <sharma.pratik2016@gmail.com>
2021-04-05 20:52:57 +0530

    fetching and post request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AudioSpectrum from './AudioAnnotation/AudioSpectrum';
 
 import './App.css'
 
-function App() {
-  
 
+
+function App() {
+  const [url, setUrl] = useState('')
+  const [spectogram, setSpectogram] = useState('')
+  const [filedata, setFiledata] = useState({})
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    fetch("http://xn--11by0j.com:8000/api/v1/get_data")
+    .then(
+      res => res.json()
+    ).then(result => {
+      console.log(result)
+      setUrl(result.sound_url)
+      setSpectogram(result.spectogram_url)
+      setFiledata({
+        table: result.table,
+        index: result.index,
+        serial: result.serial
+      })
+      setLoading(false)
+    },)
+  },[url])
   return (
-    <div className="App">      
-      <AudioSpectrum />
+    <div className="App"> 
+    {!loading?
+    <AudioSpectrum url={url} spectogram={spectogram} filedata={filedata} />: null  
+  }
+
       
     </div>
   );
--- src/AudioAnnotation/AudioSpectrum.js
+++ src/AudioAnnotation/AudioSpectrum.js
@@ -12,6 +12,9 @@ import Image from './../audio/1/1/110.png'
 import audiofile from './../audio/1/1/110.wav'
 
 
+
+
+
 class AudioSpectrum extends Component {
     static randomColor = (gradient = 0.5) =>
         `
@@ -25,13 +28,17 @@ class AudioSpectrum extends Component {
         zoom: 0,
         cursorTime: 0,
         url: this.props.url,
+        spectogram: this.props.spectogram,
+        table:this.props.filedata.table,
+        index: this.props.filedata.index,
+        serial: this.props.filedata.serial,
         start: 0,
         end: 1,
         label: '',
-        color: '',
         duration: 0,
     }
     componentDidMount() {
+        
         const track = document.querySelector('#track');
 
         const formatTimeCallback = (seconds, pxPerSec) => {
@@ -246,10 +253,43 @@ class AudioSpectrum extends Component {
             document.body.removeChild(link);
     }
 
+    sendingFile = () => {
+        //sending post request at http://xn--11by0j.com:8000/api/v1/response_srt_web/
+        const folder =  String(this.state.table)+ "/" + String(this.state.index) + "/" + this.state.serial
+        console.log(folder)
+        const regionsList = []
+        const regions = Object.values(this.waveform.regions.list)
+            .map(region => {
+              const json = {
+                  start : region.start,
+                  end: region.end,
+                  label: region.data
+              }
+              return regionsList.push(json)
+            })
+        const data = {
+            folder: folder,
+            duration: this.state.duration,
+            regions: regions
+        }
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'},
+            body: JSON.stringify(
+                data
+            )
+        };
+        fetch('http://xn--11by0j.com:8000/api/v1/response_srt_web/', requestOptions)
+        .then(res =>{
+            res.json()
+        })
+        .then(data => console.log(data))
+    }
+
     render() {
-        const newurl = 'https://speech-ml-data.s3.amazonaws.com/hindi_pahada_new/2/9/books_read.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2KYUGO7PXK47QE7Z%2F20210403%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20210403T180433Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=c1cb50d55084d1ecb4059db4660e6ba16ca98e8add919db53acd3b0fa397bfb4'
-        const url = 'https://speech-ml-data.s3.amazonaws.com/hindi_pahada_new/2/3/books_read.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2KYUGO7PXK47QE7Z%2F20210403%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20210403T175925Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=33517957f52bf5a31964dad52046bd36dbd3cf72131882c3bc001f908a8db1ce'
-        const spectrograph = 'https://speech-ml-data.s3.amazonaws.com/hindi_pahada_new/2/test/4_spectogram.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2KYUGO7PXK47QE7Z%2F20210403%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20210403T191227Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=004df671e3dd0048b34b02ea9c037a3b50d7a90be0c928c7831699d7daa29192'
+    
         return (
             <>
                 <AudioContainer>
@@ -266,26 +306,38 @@ class AudioSpectrum extends Component {
                         Remove all
                     </button>
                     <button onClick={this.downloadFile}>download</button>
+                    <button onClick={this.sendingFile}>Sending to server</button>
                     <input type="range" id="slider" onInput={(event) => console.log(event) } />
                     <WaveformContianer>
                         <Wave id="waveform" />
-                        <audio id="track" src={audiofile} onLoadedMetadata = {event => {this.setState({duration: event.target.duration})}} />
+                        <audio id="track" src={this.state.url} onLoadedMetadata = {event => {this.setState({duration: event.target.duration})}} />
                     </WaveformContianer>
 
                     {/* <div id="Spectograph" /> */}
                 </AudioContainer>
-                <div className="LableInput">
+                <div className="labelForm">
+                    <div className="labelcontainer">
+                    <div  className="inputLabel ">
+                    <label htmlFor="start">Start</label>
                     <input value={this.state.start} step={0.1} onChange={(e) => this.setState({ start: e.target.value })} placeholder="start" type="number" />
+                    </div>
+                    <div  className="inputLabel ">
+                    <label htmlFor="end">End</label>
                     <input value={this.state.end} step={0.1} onChange={(e) => this.setState({ end: e.target.value })} placeholder="end" type="number" />
-                    <input value={this.state.color} onChange={(e) => this.setState({ color: e.target.value })} placeholder="color" type="string" />
+                    </div>
+                    <div  className="inputLabel ">
+                    <label htmlFor="label">Label</label>
                     <input value={this.state.label} onChange={(e) => this.setState({ label: e.target.value })} placeholder="label" type="string" />
+                    </div>
+                    </div>
+                    
                     <button onClick={this.addRegion}>
                         Add a new region
                 </button>
                 </div>
 
                 {/* <img src={Image} height="300px" width="100%" alt="spectrograph" /> */}
-                <img src={spectrograph} height="300px" width="100%" alt="newdata"/>
+                <img src={this.state.spectogram} height="300px" width="100%" alt="newdata"/>
                 
                 <Timeline id="timeline" />
 
